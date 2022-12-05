283e2d29e8efc3d0a4308bccf5d61f02699b099a
Boris Shaporov <boris.tvink1@gmail.com>
2022-12-05 21:23:45 +0300

    init

---
+++ src/test/java/org/myself/ApplicationTest.java
@@ -0,0 +1,12 @@
+package org.myself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    public void contextLoad() {
+    }
+}
